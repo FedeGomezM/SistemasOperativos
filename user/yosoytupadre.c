516547be78823d7c06870995c16594c11121dffa
FedeGomez <fedgomez@alumnos.uai.cl>
2023-11-09 10:08:59 -0300

    Cambios en yosoytupadre en Prints

--- user/yosoytupadre.c
+++ user/yosoytupadre.c
@@ -6,7 +6,11 @@ int main(void) {
     printf("Yo soy un hijo - dijo el proceso %d\n", getpid());
     printf("Yo soy tu padre - dijo el proceso %d\n", getppid());
     for (int i = 0; i < 5; i++){
-        printf("Mi ancestro número %d es %d - dijo el proceso %d\n", i, getancestor(i), getpid());    
+        if (getancestor(i) != -1){
+            printf("Mi ancestro número %d es %d - dijo el proceso %d\n", i, getancestor(i), getpid());
+        } else {
+            printf("No tengo ancestro número %d (Return: %d) - dijo el proceso %d\n", i, getancestor(i), getpid());
+        }
     };
     exit(0);
 }
